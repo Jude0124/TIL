a74ffc84c1e6554a09e132617f477cef6aa27223
Jude <legagain@gmail.com>
2022-02-17 10:49:01 +0900

    .

--- Front/React/chap01component/src/components/A06Currency.js
+++ Front/React/chap01component/src/components/A06Currency.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
-function A09Currency() {
+function A06Currency() {
+    const [data, setDAta] = useState({
+        qty : 3,
+        cost : 5,
+        inCurr : 'USD'
+    })
 
+    const currencies = ['USD', 'EUR', 'CNY'];
+    const rate = {'USD': 1, 'EUR' : 1.35, 'CNY': 6.87};
+
+    const changeData = useCallback( (evt) => {
+        setDAta({...data, [evt.target.name]: Number(evt.target.value)});
+    }, []);
+
+    const changeCurr = useCallback( (evt) => {
+        setDAta({...data, inCurr: evt.target.value});
+    }, []);
+
+
+    const total = useCallback( ()  => {
+        return currencies.map( item => {
+            const value = (data.qty * data.cost * rate['USD'] /rate[item]).toFixed(2);
+            return <span key= {item}> {item} : {value} </span>
+        })
+    })
     return (
         <div>
-            <h3>A09 Currency</h3>
+            <h3>A06 Currency</h3>
 
-            Qty: <input type="text" name="qty" className="form-control"/>
-            Cost: <input type="text" name="cost" className="form-control" />
+            Qty: <input type="text" name="qty" className="form-control"
+                    value={data.qty} onChange={changeData} />
+            Cost: <input type="text" name="cost" className="form-control" 
+                    value={data.cost} onChange={changeData} />
             Country: 
-                <select className="form-control" name="inCurr">
-                    
+                <select className="form-control" name="inCurr"
+                    value = {data.inCurr} onChange ={changeCurr}>
+                    {currencies.map( item => <option key={item}> {item}  </option>)} 
                 </select>
             <br/>
 
@@ -20,4 +46,4 @@ function A09Currency() {
     )
 }
 
-export default A09Currency;
+export default A06Currency;
