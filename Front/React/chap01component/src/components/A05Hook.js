c46f8a96dc917025b7b1c29022cf04043567bd38
Jude <legagain@gmail.com>
2022-02-17 10:48:54 +0900

    .

--- Front/React/chap01component/src/components/A05Hook.js
+++ Front/React/chap01component/src/components/A05Hook.js
@@ -1,24 +1,57 @@
 
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 const A05Hook = () => {
   
     const [data, setData] = useState({
         num : 0,
-        str : ''
+        str : '',
+        name : ''
     });
-    
-    const changeData = (evt) => setData({...data, [evt.target.name]: evt.target.num})
-
     const [today, setToday] = useState(new Date());
+    
+    // 값이 유지되는 변수를 정의 할 목적으로 사용한다. 화명 갱신 작업은 이루어 지지 않는다.
+    const cnt = useRef(1);
+    
+    // DOM 객체를 참조할 목적으로도 사용한다.
+    const inputElem = useRef();
 
-    setTimeout ( () => {
-        setToday(new Date());
+    // setTimeout ( () => {
+    //     setToday(new Date());
 
-    }, 3000);
+    // }, 3000);
 
+    useEffect(() => {
+        setTimeout(() => {
+            setToday(new Date());
+        }, 3000);
+    }, [data]);
 
-    return (
+    useEffect(()=> {
+        console.log(inputElem.current);             //JavaScript 객체
+        inputElem.current.style.border = '1px solid orange';
+    }, []);
+
+    // 함수
+    const changeData = useCallback(
+        (evt) => setData( {...data, [evt.target.name] : evt.target.value})
+   , [] );
+
+    const increase = useCallback( () => {
+        cnt.current++;
+        console.log(cnt);
+    }, []);
+
+    const decrease = useCallback( () => {
+        cnt.current--;
+        console.log(cnt);
+    }, []);
+
+    const changeName = useCallback(() =>
+        setData( {...data, name: inputElem.current.value})
+    )
+
+   return (
         <div>
             <h3>A05. useState, useEffect</h3>
 
@@ -29,15 +62,20 @@ const A05Hook = () => {
 
                 Str: {data.str}
                     <input type="text" name="str" className="form-control" 
-                        defaultValue={data.str} onchange={changeData}/><br />
+                        defaultValue={data.str} onChange={changeData}/><br />
 
-                Today: {today.toLocaleTimeString}<br />
+                Today: {today.toLocaleTimeString()}<br />
+                <br />
+                
+                Cnt : {cnt.current} <br />
+                <button className='btn btn-outline-primary btn-sm' onClick={increase}>+</button>
+                <button className='btn btn-outline-primary btn-sm' onClick={decrease}>-</button>
                 <br />
 
-                Avg: 
+                Avg: {data.name}
                     <div className="input-group">
-                        <input type="text" name="str" className="form-control"/>
-                        <button className="btn btn-outline-primary btn-sm">ADD</button>
+                        <input type="text" name="str" className="form-control" ref={inputElem}/>
+                        <button className="btn btn-outline-primary btn-sm" onClick={changeName}>ADD</button>
                     </div>
                     
                     
