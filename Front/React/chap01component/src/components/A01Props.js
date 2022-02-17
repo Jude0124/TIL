af076bde91f7c4814c89c154c9f88ef575819c39
Jude <legagain@gmail.com>
2022-02-17 09:37:37 +0900

    .

--- Front/React/chap01component/src/components/A01Props.js
+++ Front/React/chap01component/src/components/A01Props.js
@@ -3,10 +3,47 @@
 // rfce
 import React from 'react'
 
-function A01Props() {
-  return (
-    <div>A01Props</div>
+function A01Props(props) {      // props 는 React가 주입해 주는 참조 변수
+// function A01Props({ comp, name, age, ary , obj, onAdd }) { // 아래 선언과 동일   
+    const { comp, name, age, ary , obj, onAdd,  changeName , num, changeAge } = props;
+
+    return (
+    <div>
+        <h3>A01 Props Component</h3>
+
+        <div>
+            comp : {props.comp}<br />
+            Name : {props.name}<br />
+            Age : {props.age + 100}<br />
+            Array : {props.ary[0]} / {props.ary[1]} / {props.ary[5]} <br />
+            object : {props.obj.name} / {props.obj.age} / {props.obj.address} <br />
+            onAdd : {props.onAdd(10, 20)}<br />
+            Num : {props.num * 100}<br />
+            <button className='btn btn-primary btn-sm' onClick={changeName}>Change Name</button>
+            <button className='btn btn-danger btn-sm' onClick={changeAge}>Change Age</button>
+        </div>
+
+        <hr />
+
+
+        <div>
+            comp : {comp}<br />
+            Name : {name}<br />
+            Age : {age + 100}<br />
+            Array : {ary[0]} / {ary[1]} / {ary[5]} <br />
+            object : {obj.name} / {obj.age} / {obj.address} <br />
+            onAdd : {onAdd(10, 20)}<br />
+            Num : {num *10}<br />
+            <button className='btn btn-primary btn-sm' onClick={changeName}>Change Name</button>
+        </div>
+
+    </div>
+
+ 
   )
 }
 
 export default A01Props
+A01Props.defaultProps = {
+    num : 1000
+}
